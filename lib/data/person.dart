e03ab01539bcd319b5d5a3d11cde552c41fe7347
Keatr0n <gingerepicness@gmail.com>
2020-11-01 21:36:16 +1000

    minor changes

--- lib/data/person.dart
+++ lib/data/person.dart
@@ -11,10 +11,10 @@ class Person {
   @HiveField(1)
   String lastName;
 
-  // @HiveField(2) // can we just use a list of maps?
+  // @HiveField(2) // can we just use a map?
   // List<Datablocks> datablocks;
 
-  @HiveField(2)
+  @HiveField(2) // I mean, data blocks might be easier so we may switch to that in the future.
   Map<String, String> datablocks;
 
   Person({@required this.firstName, @required this.lastName});
